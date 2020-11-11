e9e52c21d2ab7ef2f1d1e581f026c9df4775031c
Naman <patwanaman@gmail.com>
2020-11-11 06:38:35 +0000

    folder commit

---
+++ rgfolder/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group" {
+  description = "The name of your Azure Resource Group."
+}
+
+variable "location" {
+  description = "The region where the virtual network is created."
+}
